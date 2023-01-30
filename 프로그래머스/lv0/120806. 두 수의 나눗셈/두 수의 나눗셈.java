a3366977580c8763b861c18ec04df74a354c119a
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-01-31 01:07:34 +0900

    Update 두 수의 나눗셈.java

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,6 +1,15 @@
+// 방법 1
 class Solution {
     public int solution(int num1, int num2) {
         double answer = (double)num1/num2*1000;
         return (int)answer;
+    }
+}
+
+
+// 방법 2
+class Solution {
+    public int solution(int num1, int num2) {
+        return (int)(((double)num1/num2)*1000);
     }
 }
