393a9e7a1dce7f6ae3c6e0141950fb74878ce5de
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-01-31 01:21:15 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for(int i = 0 ; i <= n ; i++) {
-            if (i%2 == 0) {
-                 answer += i;               
-            }          
-        }       
+        
+        for (int i = 2; i <= n; i+=2) {
+            answer += i;
+        }
+        
         return answer;
     }
 }
