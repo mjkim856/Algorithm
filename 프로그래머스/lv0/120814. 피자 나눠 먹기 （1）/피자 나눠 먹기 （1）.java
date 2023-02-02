6376c6bd1cc1cf0acc01d971c49293ad6da40efa
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-02-03 08:49:57 +0900

    Update 피자 나눠 먹기 （1）.java

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,3 +1,5 @@
+// 풀이 1 (내 방법)
+// 사람이 7의 배수라면 몫만큼의 피자가 필요하고, 7의 배수가 아니라면 몫+1개의 피자가 필요하다.
 class Solution {
     public int solution(int n) {
         int answer = 0;
@@ -7,5 +9,24 @@ class Solution {
             answer = n/7 + 1;
         }
         return answer;
+    }
+}
+
+
+// 풀이 2
+// 위의 방법을 삼항연산자로 표현했다.
+class Solution {
+    public int solution(int n) {
+        int answer = (n%7 == 0 ? n/7 : n/7+1);
+        return answer;
+    }
+}
+
+
+// 풀이 3
+// 세상은 넓고 천재는 많다
+class Solution {
+    public int solution(int n) {
+        return (n + 6) / 7;
     }
 }
