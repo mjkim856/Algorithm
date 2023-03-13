4c6bf739a74abf77265aab9e423f1ff7cd328585
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-03-13 23:50:17 +0900

    Update 머쓱이보다 키 큰 사람.java

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -1,13 +1,23 @@
+// 내 풀이
 class Solution {
     public int solution(int[] array, int height) {
-        int answer = 0;
-        
+        int answer = 0;       
         for(int a : array) {
             if (a > height) {
                 answer += 1;
             }
+        }       
+        return answer;
+    }
+}
+
+// 이렇게 하면 더 간단하게 적을 수 있다!
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        for(int a : array) {
+            if(height < a) answer++;
         }
-        
         return answer;
     }
 }
