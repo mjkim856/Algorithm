d52de6e9e2f97b15724c92e2a11cb0b21232f9c1
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-03-13 23:47:54 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        
+        for(int a : array) {
+            if (a > height) {
+                answer += 1;
+            }
+        }
+        
+        return answer;
+    }
+}
