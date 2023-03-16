98ea46444c4e08c9863c657168e70402f37d52b3
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-03-16 21:55:02 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.04 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -4,10 +4,11 @@ class Solution {
         // 2. for문을 돌리는데 :
         // 2-1. i의 범위는 numbers.length-1 이어야 한다. 
         //      length는 1부터 시작하지만, 배열은 0부터 시작하기 때문.
+        //      ! 애초에 length를 number가 아니라 answer로 주면 해결되는 문제였다 !
         // 2-2. numbers[i]번째 값에 * 2를 하여 answer[i]번째에 넣는다.
         // 3. answer를 반환한다.
         int[] answer = new int[numbers.length];
-        for(int i = 0; i <= numbers.length-1; i++) {
+        for(int i = 0; i <= answer.length-1; i++) {
             answer[i] = numbers[i] * 2;
         }
         return answer;
