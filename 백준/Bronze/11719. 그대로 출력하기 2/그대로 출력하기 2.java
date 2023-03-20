ccb9420f13b73c8ffe2a3960b3d87129ecba4d16
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-03-20 23:43:02 +0900

    Update 그대로 출력하기 2.java

--- 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -1,5 +1,6 @@
 import java.lang.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String args[]) throws IOException{
@@ -7,6 +8,13 @@ public class Main {
         String str;
 
         while((str=br.readLine()) != null) {
+            System.out.println(str);
+        }
+        
+        // Scanner의 nextLine()을 사용해서 푸는 방법
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()) {
+            str = sc.nextLine();
             System.out.println(str);
         }
 
