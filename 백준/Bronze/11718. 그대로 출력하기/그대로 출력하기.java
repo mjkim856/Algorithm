8d517214d5d3d9fac42c1e9280b589e2fd2e7cbb
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-03-20 23:34:12 +0900

    Update 그대로 출력하기.java

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -1,3 +1,5 @@
+// 방법 1. Scanner 사용하기
+// while, hasNext(), nextLine()
 import java.lang.*;
 import java.io.*;
 
@@ -9,5 +11,20 @@ public class Main {
         while((str=br.readLine()) != null) {
             System.out.println(str);
         }
+    }
+}
+
+// 방법 2. BufferedReader 사용하기
+// while, readLine() != null
+import java.util.*;
+
+class Main {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            String str = sc.nextLine();
+            System.out.println(str);
+        }
+        sc.close();
     }
 }
