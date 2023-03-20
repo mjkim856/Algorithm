ed9b3843b16b7e90c944edd33574afa86dc83f65
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-03-20 11:30:32 +0900

    Update 아주 간단한 계산기.java

--- SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.java
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.java
@@ -8,10 +8,18 @@ class Solution
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
+     
+        // 방법 1
+        // 메모리: 20,124 kb, 실행시간: 134 ms
         System.out.println(a+b);
         System.out.println(a-b);
         System.out.println(a*b);
         System.out.println(a/b);
+     
+        // 방법 2
+        // 메모리: 21,072 kb, 실행시간: 128 ms
+        System.out.printf("%d\n%d\n%d\n%d", a+b, a-b, a*b, a/b);
+     
         sc.close();
     }
 }
