72118ef5cab47ffc9ff890dcf595056788cb404d
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-03-22 01:54:01 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13860 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,13 @@
+public class Main {
+    static long func(long a, long b) {
+        return (a+b)*(a-b);
+    }
+    public static void main(String args[]){
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
