b9dd8e889b4c5b292c8bd2e07013f7e7d5db2cb8
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-03-22 01:52:33 +0900

    [Bronze V] Title: 이상한 기호, Time: 132 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -2,15 +2,16 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int func(int a, int b) {
+    static long func(long a, long b) {
         return (a+b)*(a-b);
     }
     public static void main(String args[]) throws IOException {
-        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int result = func(a, b);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long result = func(a, b);
 
         System.out.println(result);
     }
