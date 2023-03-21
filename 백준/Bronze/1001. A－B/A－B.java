217ae3349e167b2d2f42805c44c7515f7e4546f3
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-03-21 21:34:02 +0900

    [Bronze V] Title: A-B, Time: 132 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -1,11 +1,27 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String args[]) throws IOException {
+        /*
+        풀이 1. Scanner 사용
+        간단한 숫자는 형변환 안 해도 돼서 편하다!
+
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-
         System.out.println(a-b);
+        sc.close();
+        */
+
+        // 풀이 2. BufferedReader, StringTokenizer 사용
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a - b);
+
+        br.close();
     }
 }
