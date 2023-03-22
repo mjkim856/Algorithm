34c1bab1f2bae5b7a290e85b9ea7328b4837e478
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-03-22 11:44:26 +0900

    Update 서랍의 비밀번호.java

--- SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.java
+++ SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.java
@@ -19,3 +19,18 @@ class Solution
         System.out.println(count);
 	}
 }
+
+// 반복문 안 쓰고 그냥 빼도 됨
+import java.util.*;
+ 
+class Solution
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int P, K;
+        P=sc.nextInt();
+        K=sc.nextInt();
+        System.out.println(P - K + 1);
+    }
+}
