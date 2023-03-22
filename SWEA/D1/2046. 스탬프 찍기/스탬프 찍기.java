899832c4fb123aa5f039dd499624005db6e837ba
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-03-22 11:36:52 +0900

    [D1] Title: 스탬프 찍기, Time: 125 ms, Memory: 20,724 KB -BaekjoonHub

---
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++) {
+            System.out.print("#");
+        }
+    }
+}
