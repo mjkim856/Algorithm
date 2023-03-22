855f5fd1167aa67bdb692c16b426824838a12d3c
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-03-22 11:41:43 +0900

    [D1] Title: 서랍의 비밀번호, Time: 122 ms, Memory: 20,556 KB -BaekjoonHub

---
+++ SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution
+{
+	public static void main(String args[]) 
+	{
+        Scanner sc = new Scanner(System.in);
+        int P, K;
+        P=sc.nextInt();
+        K=sc.nextInt();
+        System.out.println(P - K + 1);
+	}
+}
