21cbd81cff2bd7f99f6fbf7470fa400b8ab2d6e5
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-03-24 15:34:55 +0900

    [D2] Title: 초심자의 회문 검사, Time: 132 ms, Memory: 20,304 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -1,38 +1,27 @@
-import java.util.*;
-import java.lang.*;
+import java.util.Scanner;
 
-class Solution {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int N;
-        N = sc.nextInt();       // 테스트 케이스의 개수
-        sc.nextLine();          // nextInt() 하고 바로 값 불러오면 "" 반환한다. 다음 줄로 넘겨줘야 한다.
+public class Solution {
 
-for(int i = 1; i <= N; i++) {
-            int result = 0;
-            String str = sc.nextLine().trim();
-            int idxLength = str.length() - 1;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt(); // 테스트 케이스 개수 입력
+        scanner.nextLine(); // 개행 문자 제거
 
-            if(idxLength%2 == 0) {
-                for(int j = 0; j < idxLength/2; j++) {
-                    if(str.charAt(j) != str.charAt(idxLength - j)) {
-                        break;
-                    } else {
-                        result = 1;
-                    }
-                }
-            } else {
-                for(int j = 0; j <= idxLength/2; j++) {
-                    if(str.charAt(j) != str.charAt(idxLength - j)) {
-                        break;
-                    } else {
-                        result = 1;
-                    }
+        for (int test_case = 1; test_case <= T; test_case++) {
+            String word = scanner.nextLine();
+            boolean isPalindrome = true;
+
+            for (int i = 0; i < word.length() / 2; i++) {
+                if (word.charAt(i) != word.charAt(word.length() - i - 1)) {
+                    isPalindrome = false;
+                    break;
                 }
             }
-            System.out.printf("#%d %d\n", i, result);
+
+            System.out.printf("#%d %d\n", test_case, isPalindrome ? 1 : 0);
         }
 
-         sc.close();
+        scanner.close();
     }
+
 }
