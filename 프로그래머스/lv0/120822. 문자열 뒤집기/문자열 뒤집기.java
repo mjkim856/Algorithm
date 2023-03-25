a41186b5ca859af8575f2dcfcb6811df57c956f1
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-03-25 20:12:02 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.05 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String my_string) {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(my_string);
+        sb.reverse();
+        
+        return sb.toString();
+
+    }
+}
+
+/*
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+
+        for (int i = my_string.length()-1; i < 0 ; i--) {
+            answer += my_string.charAt(i);
+        }
+        
+        return answer;
+    }
+}
+*/
