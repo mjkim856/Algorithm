6d6e0133decbd19bfc10311f50fe0eff87c2e223
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-03-27 04:03:39 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.33 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,24 +1,44 @@
+// StringBuilder의 append(), reverse() 사용!
+// String의 .valueOf(n).split(""); 사용!
 import java.util.*;
 
 class Solution {
     public long solution(long n) {
-        long answer = 0;
-        String temp = "";
+        String arr[] = String.valueOf(n).split("");
+        Arrays.sort(arr);
 
-        String str = String.valueOf(n);
-        String[] arr = new String[str.length()];
-
-        for (int i = 0; i < str.length(); i++) {
-            arr[i] = String.valueOf(str.charAt(i));
-        }
-
-        Arrays.sort(arr, Collections.reverseOrder());
-
+        StringBuilder sb = new StringBuilder();
         for(String a:arr) {
-            temp += a;
+            sb.append(a);
         }
-
-        answer = Long.parseLong(temp);
-        return answer;
+        
+        return Long.parseLong(sb.reverse().toString());
     }
 }
+
+// -
+
+// import java.util.*;
+
+// class Solution {
+//     public long solution(long n) {
+//         long answer = 0;
+//         String temp = "";
+
+//         String str = String.valueOf(n);
+//         String[] arr = new String[str.length()];
+
+//         for (int i = 0; i < str.length(); i++) {
+//             arr[i] = String.valueOf(str.charAt(i));
+//         }
+
+//         Arrays.sort(arr, Collections.reverseOrder());
+
+//         for(String a:arr) {
+//             temp += a;
+//         }
+
+//         answer = Long.parseLong(temp);
+//         return answer;
+//     }
+// }
