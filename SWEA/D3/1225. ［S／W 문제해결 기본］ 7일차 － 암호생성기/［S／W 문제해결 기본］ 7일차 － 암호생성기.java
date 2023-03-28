5d22322e353729ee817187b8a53b3d15c2c275e2
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-03-28 13:32:06 +0900

    Update ［S／W 문제해결 기본］ 7일차 － 암호생성기.java

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -98,3 +98,50 @@ public class Solution {
         }
     }
 }
+
+-
+  
+원래 푼 방식.
+코드 위치를 수정함.
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < 10; i++) {
+            int n = sc.nextInt();
+            sc.nextLine();
+            q.clear();
+            int minus = 1;
+
+            for (int j = 0; j < 8; j++) {
+                q.add(sc.nextInt());
+            }
+
+            while(true) {
+                int temp = q.poll() - minus;
+                minus++;
+                if(temp <= 0) {
+                    temp = 0;
+                }
+                q.add(temp);
+
+                if(minus == 6) {
+                    minus = 1;
+                }
+
+                if(temp == 0) {
+                    System.out.print("#" + n + " ");
+                    for (int j = 0; j < 8; j++) {
+                        System.out.print(q.poll() + " ");
+                    }
+                    System.out.println();
+                    break;
+                }
+            }
+            System.out.printf("");
+        }
+    }
+}
