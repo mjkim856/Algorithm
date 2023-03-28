fe7dcaa6b7c1f197fb16038caaf1bdbda87dca96
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-03-28 13:26:59 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 131 ms, Memory: 24,712 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,42 +1,57 @@
 import java.util.*;
+import java.io.*;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+         // 빠르게 불러오기위한 BufferedReader 사용
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // Queue 정의
         Queue<Integer> q = new LinkedList<>();
 
-        for (int i = 0; i < 10; i++) {
-            int n = sc.nextInt();
-            sc.nextLine();
+        // 테스트케이스가 10개라 10번 반복한다.
+        for (int i = 1; i <= 10; i++) {
+            // #i 형태로 넣을 거라 첫번째는 뛰어넘는다.
+            br.readLine();
+            // Queue 초기화 시킨다.
             q.clear();
+
+            // br.readLine()을 사용한 한 줄을 StringTokenizer에 넣는다.
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            // 8개의 숫자가 주어지므로 8만큼 반복한다.
+            for (int j = 0; j < 8; j++) {
+                q.add(Integer.parseInt(st.nextToken()));
+            }
+
+            // 감소시킬 숫자 minus에 담음
             int minus = 1;
 
-            for (int j = 0; j < 8; j++) {
-                q.add(sc.nextInt());
-            }
-
             while(true) {
-                int temp = q.poll() - minus;
-                minus++;
+                // 만약 숫자가 5 초과라면 1로 초기화시킨다.
+                if(minus > 5) minus = 1;
+
+                // temp(임시값) = Queue.poll() - minus
+                // minus++는 이 식이 실행된 후 + 해준다.
+                int temp = q.poll() - minus++;
+
+                // 만약 temp가 0 이하라면?
+                // Queue에 0을 더한 후, while을 빠져나간다.
                 if(temp <= 0) {
-                    temp = 0;
-                }
-                q.add(temp);
-
-                if(minus == 6) {
-                    minus = 1;
-                }
-
-                if(temp == 0) {
-                    System.out.print("#" + n + " ");
-                    for (int j = 0; j < 8; j++) {
-                        System.out.print(q.poll() + " ");
-                    }
-                    System.out.println();
+                    q.add(0);
                     break;
                 }
+
+                // temp가 0 초과인 경우는 위 과정을 반복한다.
+                q.add(temp);
             }
-            System.out.printf("");
+
+            // 정답 출력하는 부분 : #1 6 2 2 9 4 1 3 0 의 형태이다.
+            System.out.print("#" + i + " ");
+
+            for (int j = 0; j < 8; j++) {
+                System.out.print(q.poll() + " ");
+            }
+            System.out.println();
         }
     }
 }
