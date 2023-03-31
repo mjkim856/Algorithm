003b9ccab8902593418172221bda51d17d885d8f
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-03-31 12:05:19 +0900

    Update 곱셈.java

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -2,6 +2,14 @@ import java.io.*;
 
 public class Main {
         public static void main(String[] args) throws Exception {
+//            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//            int a = Integer.parseInt(br.readLine());
+//            String b = br.readLine();
+//
+//            System.out.println(a*(b.charAt(2) - '0'));
+//            System.out.println(a*(b.charAt(1) - '0'));
+//            System.out.println(a*(b.charAt(0) - '0'));
+//            System.out.println(a*Integer.parseInt(b));
 
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             int a = Integer.parseInt(br.readLine());
