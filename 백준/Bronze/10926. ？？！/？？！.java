77064e1c8c37237be697121ddb3726c07e360410
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-03-31 11:18:25 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+class Main{
+	public static void main(String args[]) throws IOException{
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	System.out.println(br.readLine() + "??!");
+	}
+
+}
