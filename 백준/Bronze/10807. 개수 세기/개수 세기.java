7d9605c2c726e709e17ae0f5d66f903488004d14
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-04-01 16:20:25 +0900

    Update 개수 세기.java

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -7,6 +7,59 @@ class Main{
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(br.readLine());
+        int cnt = 0;
+   
+        for(int i = 0; i < n; i++) {
+            if(Integer.parseInt(st.nextToken()) == v) {
+                cnt++;
+            }
+        }
+        
+        System.out.print(cnt);
+    }
+}
+
+-
+    
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(list);
+
+        int v = Integer.parseInt(br.readLine());
+        int cnt = 0;
+
+        for(int i = 0; i < list.size(); i++) {
+            if(list.get(i) > v)
+                break;
+            if(list.get(i) == v)
+                cnt++;
+        }
+        System.out.print(cnt);
+    }
+}
+
+-
+    
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         ArrayList<Integer> list = new ArrayList<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -25,3 +78,4 @@ class Main{
         System.out.print(cnt);
     }
 }
+
