93d48e9e20238b02a4063fbdd39a6c236ffc125f
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-04-01 16:10:03 +0900

    [Bronze V] Title: 개수 세기, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -7,17 +7,21 @@ class Main{
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String str = br.readLine();
-        String v = br.readLine();
-        int cnt = 0;
-        StringTokenizer st = new StringTokenizer(str, " ");
-        
-        while(st.hasMoreTokens()) {
-            if(st.nextToken().equals(v)) {
-                cnt++;
-            }
+        ArrayList<Integer> list = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
         }
-        
+        Collections.sort(list);
+
+        int v = Integer.parseInt(br.readLine());
+        int cnt = 0;
+
+        for(int i = 0; i < list.size(); i++) {
+            if(list.get(i) == v)
+                cnt++;
+        }
         System.out.print(cnt);
     }
 }
