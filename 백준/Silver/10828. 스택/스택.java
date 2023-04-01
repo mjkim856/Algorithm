a8e96364a7bd3197e598c1942e7186034bca32cf
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-04-02 02:20:00 +0900

    Update 스택.java

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,3 +1,11 @@
+처음으로 시간 초과가 발생한 문제이다.
+푸는 방법이야 많지만, 먼저 Scanner를 사용했다. 역시나 시간 초과 ...
+다음으로는 BufferedReader를 사용했다. 무사 통과.
+다음으로는 BufferedReader와 StringBuilder를 사용했다. 사용 시간이 절반으로 줄엇다. 뿌듯.
+앞으로는 StringBuilder에 익숙해지도록 하자.
+
+-
+
 import java.util.*;
 import java.io.*;
 
@@ -25,4 +33,33 @@ class Main {
         }
         System.out.println(sb.toString());
 	}
+}
+
+-
+	
+import java.util.*;
+import java.io.*;
+
+class Main {
+	public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> st = new Stack<>();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            String[] s = br.readLine().split(" ");
+            if(s[0].equals("push")) {
+                st.push(Integer.parseInt(s[1]));
+            } else if(s[0].equals("pop")) {
+                System.out.println(st.empty() ? -1 : st.pop());
+            } else if(s[0].equals("top")) {
+                System.out.println(st.empty() ? -1 : st.peek());
+            } else if(s[0].equals("size")) {
+                System.out.println(st.size());
+            } else if(s[0].equals("empty")) {
+                System.out.println(st.empty() ? 1 : 0);
+            }
+        }
+    }
 }
