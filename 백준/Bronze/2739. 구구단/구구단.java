d78667a661091491bd8be33c3645f8dfd6068912
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-04-03 23:39:51 +0900

    [Bronze V] Title: 구구단, Time: 136 ms, Memory: 14516 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        for(int j = 1; j <= 9; j++) {
+            System.out.printf("%d * %d = %d\n", n, j, n*j);
+        }        
+    }
+}
