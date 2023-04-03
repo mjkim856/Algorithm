40da7a28bc66a6f44274561cd186739b2b884047
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-04-03 23:47:09 +0900

    Update 구구단.java

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -13,3 +13,21 @@ class Main{
         }        
     }
 }
+
+-
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        for(int j = 1; j <= 9; j++) {
+            sb.append(n + " * " + j + " = " + n*j + "\n");
+        }     
+        System.out.println(sb);
+    }
+}
