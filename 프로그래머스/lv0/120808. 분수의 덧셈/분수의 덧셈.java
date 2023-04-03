83bfc3e3a6c554deb389737d43c7d5f1a82507fd
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-04-03 17:48:24 +0900

    Update 분수의 덧셈.java

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -2,23 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-        int gcd = gcd(denom1, denom2);          // 최대공약수
-        int lcm = (denom1 * denom2) / gcd;      // 최소공배수
+        int gcd = gcd(denom1, denom2);              // 최대공약수
+        int lcm = (denom1 * denom2) / gcd;          // 최소공배수, 최종값 분모
         
-        int numer3 = (numer1 * (lcm/denom1)) + (numer2 * (lcm/denom2));
-        int denom3 = lcm;           
+        int numer3 = (numer1 * (lcm/denom1)) + (numer2 * (lcm/denom2)); // 최종값 분자        
                                                 
-        int gcd2 = gcd(numer3, denom3);          // 최대공약수                                         
- 
-        int[] answer = {numer3/gcd2, lcm/gcd2} ;
+        int gcd2 = gcd(numer3, lcm);                // 기약분수가 아닐 수 있음므로 두 수의 최대공약수를 구해준다
+        int[] answer = {numer3/gcd2, lcm/gcd2} ;    // 최대공약수로 나눈 값이 최최종분모, 최최종분자이다.
         
         return answer;
     }
     
     static int gcd(int a, int b) {
-        if(b == 0) {
-            return a;
-        }
+        if(b == 0) return a;
         return gcd(b, a%b);
     }
 }
