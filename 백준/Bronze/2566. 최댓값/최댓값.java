05d35b50a535b43f14fa63f92d7f4c44a591bbf4
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-04-04 02:33:08 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -6,32 +6,26 @@ public class Main {
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st1;
-        StringTokenizer st2;
         int max = -1;
         int row = 0;
         int col = 0;
+        int arrt = 0;
 
-        int[][] arrt1 = new int[9][9];
-        
         for(int i = 0; i < 9; i++) {
             st1 = new StringTokenizer(br.readLine());
             for(int j = 0; j < 9; j++) {
-                arrt1[i][j] = Integer.parseInt(st1.nextToken());
-            }
-        }
-
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
-                if(arrt1[i][j] > max) {
-                    max = arrt1[i][j];
+                arrt = Integer.parseInt(st1.nextToken());
+                if(arrt > max) {
+                    max = arrt;
                     row = i+1;
                     col = j+1;
                 }
             }
         }
         
-        System.out.println(max);
-        System.out.println(row + " " + col);
+        StringBuilder sb = new StringBuilder();
+        sb.append(max).append("\n").append(row).append(" ").append(col);
+        System.out.println(sb);
 
         br.close();
     }
