684a76abd811e6a6b80148956ad30bfb0f92bd8c
mjkim856 <107450834+mjkim856@users.noreply.github.com>
2023-04-03 12:20:11 +0900

    [Bronze V] Title: 시험 성적, Time: 216 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,25 @@
+// 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+
+import java.util.*;
+
+public class Main{
+	public static void main(String args[]){
+	    Scanner sc = new Scanner(System.in);
+	    int a = sc.nextInt();
+        
+        switch(a/10) {
+            case 10:
+            case 9: System.out.print("A");
+                break;
+            case 8: System.out.print("B");
+                break;
+            case 7: System.out.print("C");
+                break;
+            case 6: System.out.print("D");
+                break;
+            default: System.out.print("F");
+                break;
+        }
+        sc.close();
+	}
+} 
