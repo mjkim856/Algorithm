f2c7957b44b755bfcaba2bc6007c68a24dae5ff4
Minji Kim <107450834+mjkim856@users.noreply.github.com>
2023-04-06 11:26:56 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -5,9 +5,7 @@ public class Main {
 
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         char c = br.readLine().charAt(0);
-        sb.append((int)c);
-        System.out.print(sb);
+        System.out.print((int)c);
     }
 }
