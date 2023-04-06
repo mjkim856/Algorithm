da9f69e58ace6b7e575ed05077da85688f9c2074
Minji Kim <107450834+mjkim856@users.noreply.github.com>
2023-04-06 22:37:50 +0900

    [Bronze II] Title: 문자열 반복, Time: 112 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,11 +1,12 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 class Main {
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st1;
+
         int n = Integer.parseInt(br.readLine());
 
         for(int i = 0; i < n; i++) {
@@ -20,6 +21,6 @@ class Main {
             }
             sb.append("\n");
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
