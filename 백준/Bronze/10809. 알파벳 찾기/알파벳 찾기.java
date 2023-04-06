16980b3f219806033d6248fc0c0a7d203677d0c0
Minji Kim <107450834+mjkim856@users.noreply.github.com>
2023-04-06 13:28:07 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -5,15 +5,10 @@ public class Main {
 
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        // 알파벳 숫자 26을 가지는 배열을 생성한다.
-        // 배열에 -1을 모두 넣는다.
-        // 반복문을 사용한다.
-        // 한 글자씩 읽어온다. 
-        // arr[(int)charAt(i) - 96] = i 씩 해준다.
-        // 배열 출력하기
+        StringBuilder sb = new StringBuilder();
         
         int[] arr = new int[26];
+        
         for(int i = 0; i < 26; i++) {
             arr[i] = -1;
         }
@@ -22,12 +17,13 @@ public class Main {
         
         for(int i = 0; i < str.length(); i++) {
             int a = (int)str.charAt(i) - 97;
-            if(a != -65 && arr[a] == -1) arr[(int)str.charAt(i) - 97] = i;
+            if(arr[a] == -1) arr[(int)str.charAt(i) - 97] = i; 
         }
-
         
         for(int a:arr) {
-            System.out.print(a + " ");
+            sb.append(a).append(" ");
         }
+        
+        System.out.println(sb);
     }
 }
