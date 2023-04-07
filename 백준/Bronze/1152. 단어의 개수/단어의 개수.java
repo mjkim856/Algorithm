9878da6d2cb04915e459fa6c758171a6de0ea471
Minji Kim <107450834+mjkim856@users.noreply.github.com>
2023-04-07 09:28:41 +0900

    Update 단어의 개수.java

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -11,3 +11,21 @@ public class Main {
 
     }
 }
+
+-
+    
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        if(str.equals(" ")) {
+            System.out.println("0");
+        } else {
+            String[] arr = str.trim().split(" ");
+            System.out.println(arr.length);
+        }
+    }
+}
