24918246156e49f20a2710e7b35f61449334dae8
Minji Kim <107450834+mjkim856@users.noreply.github.com>
2023-04-07 09:27:44 +0900

    [Bronze II] Title: 단어의 개수, Time: 240 ms, Memory: 20076 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -5,11 +5,9 @@ public class Main {
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
-        if(str.equals(" ")) {
-            System.out.println("0");
-        } else {
-            String[] arr = str.trim().split(" ");
-            System.out.println(arr.length);
-        }
+        
+        StringTokenizer st = new StringTokenizer(str);
+        System.out.println(st.countTokens());
+
     }
 }
