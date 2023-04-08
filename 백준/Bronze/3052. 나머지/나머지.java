2a5e1eacd02eb33b772f4d079ee9c2c8e2e59652
Minji Kim <107450834+mjkim856@users.noreply.github.com>
2023-04-09 01:10:30 +0900

    [Bronze II] Title: 나머지, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -4,15 +4,12 @@ import java.io.*;
 class Main {
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        List<Integer> arr = new ArrayList<>();
-        
+        HashSet<Integer> hs = new HashSet<>();
+
         for(int i = 0; i < 10; i++) {
             int num = Integer.parseInt(br.readLine());
-            if(arr.indexOf(num%42) == -1) {
-                arr.add(num%42);
-            }   
+            hs.add(num%42);
         }
-        System.out.println(arr.size());
+        System.out.println(hs.size());
     }
 }
