7b063a520bdded818278fd3d65760b1c167e7d3d
Minji Kim <107450834+mjkim856@users.noreply.github.com>
2023-04-20 10:53:14 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -1,11 +1,18 @@
 class Solution {
     public int solution(int hp) {
+        
+        
+        /*       
+        방법 1. 순서대로 차근차근
+            왜 hp%3을 더해주냐면, 어차피 1의 공격력이라 2면 개미 2마리 1이면 개미 1마리라 그렇다. 
+        
         int ant = 0;
         ant += hp/5;
         hp = hp%5;
-        
         ant += (hp/3 + hp%3);
+        */
  
-        return ant;
+        // 방법 2. 냅다 때려박기   
+        return hp/5 + hp%5/3 + hp%5%3;
     }
 }
