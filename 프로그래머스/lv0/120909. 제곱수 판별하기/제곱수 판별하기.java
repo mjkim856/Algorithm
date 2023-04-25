70f227d7e04ca4409eb8c870483913aa72f3a71b
Minji Kim <107450834+mjkim856@users.noreply.github.com>
2023-04-26 04:39:00 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.48 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,11 +1,19 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        
+        /*
+        방법 1
         for(int i = 1; i < n; i++) {
             if(i*i == n) return 1;
         }    
         
         return 2;
+        */
+        
+        // 방법 2
+        // Math의 .sqrt()를 사용한다.
+       System.out.println(Math.sqrt(25) % 1); // 0.0
+        System.out.println(Math.sqrt(20) % 1); // 0.4721354...
+        
+       return (Math.sqrt(n) % 1 == 0) ? 1 : 2;
     }
 }
