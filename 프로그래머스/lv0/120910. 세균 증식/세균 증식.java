7067577827885b368acc6e98dee7e7440252504a
Minji Kim <107450834+mjkim856@users.noreply.github.com>
2023-04-28 11:16:05 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -1,9 +1,22 @@
 class Solution {
     public int solution(int n, int t) {
+       
+        /*
+        방법 1.
         int answer = n;
+        
         for(int i = 1; i <= t; i++) {
             answer = answer*2;
         }
+        
         return answer;
+        */
+        
+        // 방법 2
+        for(int i = 0; i < t; i++) {
+            n *= 2;
+        }
+        
+        return n;
     }
 }
