78412c84f2a279cd20e7bb5089e6b055320e751c
Minji Kim <107450834+mjkim856@users.noreply.github.com>
2023-05-01 00:59:17 +0900

    Update 약수 구하기.java

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -1,22 +1,39 @@
+import java.util.*;
 class Solution {
     public int[] solution(int n) {
+        /*
+        방법 1. 배열 길이를 cnt로 구한 뒤, cnt 길이를 가진 배열을 만들고 그 안에 약수를 넣었다.
         
-            int cnt = 0;      
-            int idx = 0;
-        
-            for(int i = 1; i <= n; i++) {
-                if(n % i == 0) {
-                    cnt++;
-                }
+        int cnt = 0;      
+        int idx = 0;
+
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0) {
+                cnt++;
             }
-            
-            int[] answer = new int[cnt];
-        
-            for(int i = 1; i <= n; i++) {
-                if(n % i == 0) {
-                    answer[idx++] = i;
-                }
-            }
-            return answer;     
         }
+
+        int[] answer = new int[cnt];
+
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0) {
+                answer[idx++] = i;
+            }
+        }
+        return answer;   
+        */
+        
+        // 방법 2. ArrayList() 에 넣고 Array로 변경하기
+        // stream().mapToInt(x -> x).toArray() 를 사용함!
+        List<Integer> al = new ArrayList<>();
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0) {
+                al.add(i);
+            }
+        }        
+        
+        return al.stream()
+            .mapToInt(x -> x)
+            .toArray();  
+    }
 }
