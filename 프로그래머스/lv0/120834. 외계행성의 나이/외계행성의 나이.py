867d76f99c2d8dd74c68f96b7bde188cad10c54d
Minji Kim <107450834+mjkim856@users.noreply.github.com>
2023-06-27 18:50:41 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.py
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -1,7 +1,11 @@
+'''
 def solution(age):
     # 97 = a
     answer = ''
     for i in str(age):
         answer += chr(97+int(i))
     return answer
-    
+'''
+
+def solution(age):
+    return ''.join([chr(97 + int(i)) for i in str(age)])
