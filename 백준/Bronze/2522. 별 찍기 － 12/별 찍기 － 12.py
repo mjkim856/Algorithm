3789575fb82ef75e4d5907629eef2f613f2773c4
Minji Kim <107450834+mjkim856@users.noreply.github.com>
2024-10-17 21:42:10 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.py
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.py
@@ -1,16 +1,26 @@
 n = int(input())
 
-for i in range(n): print(" "*(n-i-1) + "*"*(i+1))
-for i in range(n-1, 0, -1): print(" "*(n-i) + "*"*i)
+# 원래는 이렇게 했는데, 아래와 같이 좀 더 범용적(?)으로도 할 수 있을 듯
+# for i in range(n): print(" "*(n-i-1) + "*"*(i+1))
+# for i in range(n-1, 0, -1): print(" "*(n-i) + "*"*i)
 
+for i in range(1, n+1): print(" "*(n-i) + "*"*i)
+for i in range(1, n): print(" "*i + "*"*(n-i))
 
 '''
 input = 3
 
+1번
   *     i = 0, * 1, 2
  **     i = 1, * 2, 1
 ***     i = 2, * 3, 0
  **     i = 2, * 2, 1
   *     i = 1, * 1, 2
 
+2번
+  *     i = 1, * 1, 2
+ **     i = 2, * 2, 1
+***     i = 3, * 3, 0
+ **     i = 1, * 2, 1
+  *     i = 2, * 1, 2
 '''
