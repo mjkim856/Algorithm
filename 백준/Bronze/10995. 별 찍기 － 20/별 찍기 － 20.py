09b3e64d1f3f47bf434c36d5630c662233901d1b
Minji Kim <107450834+mjkim856@users.noreply.github.com>
2024-10-19 22:14:47 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+for i in range(n):
+    if i%2 == 0: print("* "*n)
+    else: print(" *"*n)
+
+'''
+input = 4
+
+* * * *
+ * * * *
+* * * *
+ * * * *
+
+*
+
+* *
+ * *
+
+* * *
+ * * *
+* * *
+'''
