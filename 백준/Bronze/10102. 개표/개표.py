02a2c213d80db587f4f80250b45b4f4ea93a03de
Minji Kim <107450834+mjkim856@users.noreply.github.com>
2024-10-22 00:12:28 +0900

    [Bronze III] Title: 개표, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10102. 개표/개표.py
+++ 백준/Bronze/10102. 개표/개표.py
@@ -1,12 +1,8 @@
 n = int(input())
-li = list(input())
+v = input()
 
-a = 0
-b = 0
-
-for i in range(n):
-    if li[i] == 'A': a += 1
-    else: b += 1
+a = v.count('A')
+b = n - a
 
 if a > b: print('A')
 elif b > a: print('B')
