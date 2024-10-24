d20a340579e3a16e1c9ea667e2fabb8de26ce380
Minji Kim <107450834+mjkim856@users.noreply.github.com>
2024-10-24 23:41:07 +0900

    [Bronze II] Title: OX퀴즈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+for i in range(n):
+    ox = input()
+    add = 0
+    result = 0
+
+    for i in range(len(ox)):
+        if ox[i] == 'O':
+            add += 1
+            result += add
+        else: add = 0
+    print(result)
