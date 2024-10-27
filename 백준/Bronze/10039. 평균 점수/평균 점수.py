e92d9ff2725349334ff483e29a06613742177df2
Minji Kim <107450834+mjkim856@users.noreply.github.com>
2024-10-27 23:27:09 +0900

    Update 평균 점수.py

--- 백준/Bronze/10039. 평균 점수/평균 점수.py
+++ 백준/Bronze/10039. 평균 점수/평균 점수.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 add = 0
 
 for i in range(5):
@@ -5,4 +8,5 @@ for i in range(5):
     if n > 40: add += n
     else: add += 40
     
-print(int(add/5))
+# print(int(add/5)) 이건 실수 몫(68.0)이 나와서 형변환 해야 함
+print(add//5) # 이건 정수 몫 나와서 형변환 안 해도 댐 !!
