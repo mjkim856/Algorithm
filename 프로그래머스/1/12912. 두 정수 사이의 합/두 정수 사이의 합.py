ba5855a7ac02c19d475cb917f8ea20c491e233d4
Minji Kim <107450834+mjkim856@users.noreply.github.com>
2024-11-08 00:08:34 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 426.96 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -1,5 +1,10 @@
 def solution(a, b):
+    if a > b:
+        a, b = b, a
+    return sum(range(a, b+1))
     
+    # 첫번째 방법
+    '''
     t1 = a
     t2 = b
     
@@ -16,3 +21,4 @@ def solution(a, b):
         answer += i
 
     return answer
+    '''
